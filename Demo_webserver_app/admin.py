7296c691cbefc0b5572ff0d0b617baa6f0e963d3
Nova <booxgitboox@gmail.com>
2021-07-11 22:33:07 +0430

    add list_display for Data and NewStatus

--- Demo_webserver_app/admin.py
+++ Demo_webserver_app/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from Demo_webserver_app.models import Device,Profile,Data,NewStatus
+from Demo_webserver_app.models import ( Device,
+                                        Profile,
+                                        Data,
+                                        NewStatus,
+                                        Type
+                                        )
 
 class Device_Register(admin.ModelAdmin):
     list_display = ['name','token',"status"]
 
+class Data_Register(admin.ModelAdmin):
+    list_display = ['device','date','data']
+
+class NewStatus_Register(admin.ModelAdmin):
+    list_display = ['device','date','complated','data']
+
 admin.site.register(Device,Device_Register)
-admin.site.register(Data)
+admin.site.register(Data,Data_Register)
+admin.site.register(NewStatus,NewStatus_Register)
 admin.site.register(Profile)
-admin.site.register(NewStatus)
+admin.site.register(Type)
