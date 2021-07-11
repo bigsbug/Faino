76291449be4cda135cc0ebb7f9a415051124dcb9
Nova <booxgitboox@gmail.com>
2021-07-11 13:07:36 +0430

    test

--- Demo_webserver_app/models.py
+++ Demo_webserver_app/models.py
@@ -2,25 +2,41 @@ from django.db import models
 from django.contrib.auth.models import User
 from uuid import uuid4
 
+from django.db.models import constraints
+from django.utils import timezone, tree
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,editable=False,primary_key=True)
     name = models.fields.CharField(max_length=36,blank=False)
+    phone = models.CharField(max_length=11)
     company = models.fields.CharField(max_length=60,blank=True)
-     
+    membership = models.DateTimeField(null=True)
+
+    # class Meta:
+    #     constraints = [
+    #         models.CheckConstraint(check= models.Q(phone__gte =9),name='min_length 11'),
+    #         ]
+
     def __str__(self):
         return self.name
-    
+
+class Type(models.Model):
+    name = models.CharField(max_length=62,primary_key=True,blank=False,unique=True)
+
+    # class Meta:
+    #     constraints = [
+    #         models.CheckConstraint(check= models.Q(name__gte =4),name='min_length >= 4'),
+    #     ]
+
+    def __str__(self) -> str:
+        return self.name
 
 class Device(models.Model):
-    status = [
-        (True,'ON'),
-        (False,'OFF')
-    ]
     token = models.UUIDField(primary_key=True,default=uuid4,editable=False,unique=True)
+    type = models.ForeignKey(Type,on_delete=models.CASCADE)
     name = models.fields.CharField(max_length=36)
     description = models.fields.TextField(max_length=600,blank=True)
-    users = models.ForeignKey(Profile,related_name='Device_user',on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile,related_name='Device_user',on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
     def __str__(self):
         return self.name
