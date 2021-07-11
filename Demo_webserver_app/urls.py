ced6553aa8910d9f74f25b501e05ce2d71d813de
Nova <booxgitboox@gmail.com>
2021-07-11 22:32:16 +0430

    add new urls for autentication JWT

--- Demo_webserver_app/urls.py
+++ Demo_webserver_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 # from .consumers import Device
-from .views import index
+from .views import index , Get_Device,Get_Info_User
 urlpatterns = [
     path('home/',index),
+    path('devices/',Get_Device.as_view(),name='DEVICE'),
+    path('info_user/',Get_Info_User.as_view(),name='INFO_USER'),
 ]
