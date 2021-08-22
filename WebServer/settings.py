5f08fbfd1954fce55b359c10df2feb6ce21ed48e
bigsbug <booxgitboox@gmail.com>
2021-08-22 13:16:10 -0700

    manage static fiels

--- WebServer/settings.py
+++ WebServer/settings.py
@@ -148,7 +148,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    # STATIC_ROOT,
+    BASE_DIR,
+]
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 
