fc3dff3a317a5222346f59433896df6656927428
bigsbug <booxgitboox@gmail.com>
2022-06-10 18:43:42 -0400

    change architecture project and some old changes

---
+++ faino/AUTH_SYSTEM/permissions.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+from rest_framework.permissions import BasePermission
+from AUTH_SYSTEM.models import Permissions, Permissions_Group
+
+
+class Auto_Detect_UserDevice(BasePermission):
+
+    def has_permission(self, request, view):
+        token = view.kwargs.get('pk', None)
+        if token:
+            try:
+                user_profile = request.user.UserDevice.get(token=token)
+                # user_type = user_profile.type
+                have_permission = user_profile.type.permissions.filter(
+                    name=view.action).exists()
+                return have_permission
+            except:
+                return False
+        else:
+            return False
