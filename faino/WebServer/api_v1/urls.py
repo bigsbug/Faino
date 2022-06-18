13c867592264b5c566461b4201944c8161a101b2
bigsbug <booxgitboox@gmail.com>
2022-06-18 06:47:59 -0400

    remove APIs from WebServer

--- faino/WebServer/api_v1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.db import router
-from rest_framework.routers import DefaultRouter, SimpleRouter
-from .views import Device_API
-
-Router = DefaultRouter()
-app_name = "api"
-Router.register("device", Device_API, basename="Device")
-urlpatterns = Router.urls
