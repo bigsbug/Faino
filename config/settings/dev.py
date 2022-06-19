fefcbd85bb63c572a90a793cd878387a49257c33
bigsbug <booxgitboox@gmail.com>
2022-06-18 20:53:52 -0400

    remove unwanted text from dev settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -34,10 +34,7 @@ INSTALLED_APPS.extend(
         "drf_spectacular_sidecar",
         "faino.WebServer",
         "faino.AuthSystem",
-<<<<<<< HEAD
         "faino.API",
-=======
->>>>>>> 7da14fe (Development Files)
     ]
 )
 SITE_ID = 2
