635c0c0f67a2028a963e6ddfa0f2e1d5db09ad7a
bigsbug <booxgitboox@gmail.com>
2022-06-21 08:18:06 -0400

    chagne default settings of asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -13,7 +13,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
 django.setup()
 django_asgi_app = get_asgi_application()
 
