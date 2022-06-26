723ed0efef0629bf19de3e8f44b435f5d1a959d7
bigsbug <booxgitboox@gmail.com>
2022-06-26 14:29:38 -0400

    change name vars

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -7,4 +7,4 @@ python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput 
 
 echo -e "\n$(tput bold setaf 6)Run uvicorn server ... "
-uvicorn config.asgi:application --reload --host ${DJANGO_IP} --port ${DJANGO_PORT} --workers ${WORKERS_COUNT_UVICORN} ${EXTRA_UVICORN_ARGS}
+uvicorn config.asgi:application --reload --host ${DJANGO_IP} --port ${DJANGO_PORT} --workers ${WORKERS_COUNT} ${EXTRA_ARGS}
