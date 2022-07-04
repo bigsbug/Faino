570da5635852b2123ed6538be31452b226a108e7
bigsbug <booxgitboox@gmail.com>
2022-07-04 16:37:10 +0430

    change webserver from uvicron to gunicorn

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -6,5 +6,5 @@ echo -e "\n$(tput bold setaf 6)Makemigrations and apply migrates ... "
 python3 manage.py makemigrations --noinput 
 python3 manage.py migrate --noinput 
 
-echo -e "\n$(tput bold setaf 6)Run uvicorn server ... "
-uvicorn config.asgi:application --reload --host ${DJANGO_IP} --port ${DJANGO_PORT} --workers ${WORKERS_COUNT} ${EXTRA_ARGS}
+echo -e "\n$(tput bold setaf 6)Run gunicorn development server ... "
+gunicorn --reload -k uvicorn.workers.UvicornWorker  -b ${DJANGO_IP}:${DJANGO_PORT} --workers ${WORKERS_COUNT} ${EXTRA_ARGS}  config.asgi:application 
