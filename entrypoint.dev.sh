77c15ac448b89588a6fa8d63db5ae66a065787c6
bigsbug <booxgitboox@gmail.com>
2022-07-08 22:09:19 +0430

    change order of commands

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -1,10 +1,10 @@
+echo -e "\n$(tput bold setaf 6)Makemigrations and apply migrates ... "
+python3 manage.py makemigrations --noinput 
+python3 manage.py migrate --noinput 
 
 echo -e "\n$(tput bold setaf 6)Collect static files ..."
 python3 manage.py collectstatic --noinput 
 
-echo -e "\n$(tput bold setaf 6)Makemigrations and apply migrates ... "
-python3 manage.py makemigrations --noinput 
-python3 manage.py migrate --noinput 
 
 echo -e "\n$(tput bold setaf 6)Run gunicorn development server ... "
 gunicorn --reload -k uvicorn.workers.UvicornWorker  -b ${DJANGO_IP}:${DJANGO_PORT} --workers ${WORKERS_COUNT} ${EXTRA_ARGS}  config.asgi:application 
